c060976f5c30998f25ce71abb1815a2f038fb90a
andrewsubie <asubach@uvm.edu>
2024-10-30 12:08:26 -0400

    randomly generated formula passes quadratic test cases returning only int and float results

--- formula.py
+++ formula.py
@@ -1,95 +1,137 @@
 # class to represent a formula for finding the roots of a n-th degree polynomial.
-import numpy as np
-import copy
-import unittest
 import random
+
 class Formula:
     """
     Formula class
-    Represents a mathematical formula to find a root of a polynomial (i.e the Quadratic Formula)
-
+    Represents a mathematical formula to find a root of a polynomial (i.e., the Quadratic Formula).
     """
-    OPERATIONS = ['+', '-', '*', '/', '**','root']
-    CONSTANTS = [(1),(2),(3),(4)]
-    #options for equation coefficients
-    FIRST_DEGREE = [('a'), ('b')]
-    SECOND_DEGREE = [('a'),('b'),('c')]
+    # string representation of mathematical operators
+    OPERATIONS = ['+', '-', '*', '/', '**', 'root']
+    # options for constants in formula
+    CONSTANTS = [1, 2, 3, 4]
+    # options for equation coefficients
+    FIRST_DEGREE = ['a', 'b']
+    SECOND_DEGREE = ['a', 'b', 'c']
 
-    def __init__(self,min_length, max_length, degree_of_polynomial):
+    def __init__(self, min_length, max_length, degree_of_polynomial):
+        """
+        Constructor takes arguments to specify length of formula, degree of polynomial that it is intended to solve for the roots of
+        to load the appropriate constants into the formula
+        @param: min_length, max_length, degree_of_polynomial
+        @return: None
+        """
         self.max_length = max_length
         self.min_length = min_length
         self.degree_of_polynomial = degree_of_polynomial
         self.formula = []
         self.length_of_constants = -1
 
-        #determine which coefficent set to use for formula
+        # determine which coefficient set to use for formula based on the degree of polynomial the formula is intended to solve
+        # add apppropriate coeff. set and constants to the formula
         if self.degree_of_polynomial == 1:
-            self.formula.extend(self.FIRST_DEGREE)  # add appropriate coefficients to the formula linked list
-            self.formula.extend(self.CONSTANTS)  # add constants to the formula
+            self.formula.extend(self.FIRST_DEGREE)
+            self.formula.extend(self.CONSTANTS)
             self.length_of_constants = len(self.FIRST_DEGREE) + len(self.CONSTANTS)
 
-        if self.degree_of_polynomial == 2:
-            self.formula.extend(self.SECOND_DEGREE) # add appropriate coefficients to the formula linked list
-            self.formula.extend(self.CONSTANTS) # add constants to the formula
+        elif self.degree_of_polynomial == 2:
+            self.formula.extend(self.SECOND_DEGREE)
+            self.formula.extend(self.CONSTANTS)
             self.length_of_constants = len(self.SECOND_DEGREE) + len(self.CONSTANTS)
 
-        #randomly generate a formula in of random length in specified range
-        for _ in range(random.choice(range(self.min_length,self.max_length))):
-            operator = random.choice(self.OPERATIONS) #randomly choose operator to be added
-            first_pointer, second_pointer = random.sample(range(len(self.formula)), 2) #pick two random new elements to point to
+        # randomly generate a formula of length within allowed range
+        for _ in range(random.randint(self.min_length, self.max_length)):
+            operator = random.choice(self.OPERATIONS)  #choose operator
+            first_pointer, second_pointer = random.sample(range(len(self.formula)), 2)  # pick two random elements to point to
             self.formula.append((operator, first_pointer, second_pointer))
 
-    def evaluate_formula(self, expression):
+    def evaluate_formula_quadratic(self, expression):
+        """
+        Evaluates the formula by iterating through self.formula for a given expression, replacing the
+        coefficient variables in self.formula with actual values and returns the calculated root
+        @param: expression (5-tuple to represent the three coefficients and two roots of a quadratic equation in
+        standard form, expected in order (a, b, c, root_1, root_2) for ax2+bx+c with roots root_1, root_2)
+        @return: calulcated root (float value)
+        """
         a, b, c, root_1, root_2 = expression
-        constants = self.formula[self.length_of_constants:]
+        constants = self.formula[:self.length_of_constants] #isolate constants
 
-
-        #replace variables with actual values
+        # replace variables with actual values
         for j in range(len(constants)):
             if constants[j] == 'a':
-                self.formula[j] = a
-            if constants[j] == 'b':
-                self.formula[j] = b
-            if constants[j] == 'c':
-                self.formula[j] = c
+                constants[j] = a
+            elif constants[j] == 'b':
+                constants[j] = b
+            elif constants[j] == 'c':
+                constants[j] = c
 
-        values = self.formula[:self.length_of_constants]
-        for i in range(self.length_of_constants,len(self.formula)):
+        values = list(constants)  # list of actual values
+        #perform each operation specified in the length of the formula
+        for i in range(self.length_of_constants, len(self.formula)):
             operator, first_pointer, second_pointer = self.formula[i]
-            first_pointer = int(first_pointer)
-            second_pointer = int(second_pointer)
-
-
-            # check the pointers refer to valid indices
+            # check iff pointers return actual values
             if first_pointer < len(values) and second_pointer < len(values):
                 left_value = values[first_pointer]
                 right_value = values[second_pointer]
 
-                result = None
+                result = None #init result
+                try:
+                    if operator == '+':
+                        result = left_value + right_value
+                    elif operator == '-':
+                        result = left_value - right_value
+                    elif operator == '*':
+                        result = left_value * right_value
+                    elif operator == '/':
+                        if right_value != 0: # handle 0 division
+                            result = left_value / right_value
+                        else:
+                            pass
+                    elif operator == '**':
+                        if right_value >= 0 and left_value != 0: #handle negative exponentiation
+                            result = left_value ** right_value
+                        else:
+                            pass
+                    elif operator == 'root':
+                        if right_value != 0 and left_value > 0: #handle 0 root
+                            result = left_value ** (1 / right_value)
+                        else:
+                            pass
+                except Exception as e:
+                    print(e)
+                    values.append(values[-1])
 
-                if operator == '+':
-                    result = left_value + right_value
-                if operator == '-':
-                    result = left_value - right_value
-                if operator == '*':
-                    result = left_value*right_value
-                if operator == '/':
-                    result = left_value / right_value
-                if operator == '**':
-                    result = left_value ** right_value
-                if operator == 'root':
-                    result = left_value ** (1/right_value)
-
-                # check if result is none
-                if result is not None:
+                # append result of ea. operation to values
+                if isinstance(result, int) or isinstance(result, float):
                     values.append(result)
-        return values[-1]
-
+        # return last calculated value (root) or None if none is calculated
+        if values:
+            return values[-1]
+        else:
+            return None
 
 if __name__ == '__main__':
-    formula = Formula(5,20,2)
-    print(formula.formula)
-    formula.evaluate_formula((1,1,1,1,1))
+    TEST_CASES = 50
+    MIN_LENGTH = 5
+    MAX_LENGTH = 20
+    # quadratic test case
+    expression_1 = (1,2,1,-1,-1)
+    expression_2 = (1,7,6,-1.0,-6.0)
+    expression_3 = (1,-41,288,-9.0,-32.0)
+    results = []
+    for _ in range(TEST_CASES):
+        f = Formula(MIN_LENGTH, MAX_LENGTH, 2)
+        r1 = f.evaluate_formula_quadratic(expression_1)
+        r2 = f.evaluate_formula_quadratic(expression_2)
+        r3 = f.evaluate_formula_quadratic(expression_3)
+        print(r1, r2, r3)
+        results.append(r1)
+        results.append(r2)
+        results.append(r3)
+    for result in results:
+        assert(result is not None)
+        assert(isinstance(result,int) or isinstance(result,float))
+
 
 
 
